728e4f92b5c6d2ab6d99c2336edac01877ce8ca3
OksanaLukashyk <130361592+OksanaLukashyk@users.noreply.github.com>
2023-10-16 21:37:47 +0300

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -4,6 +4,10 @@ import { FriendListItem } from "../FriendListItem/FriendListItem"
 
 export const FriendList = ({friends}) => { 
     return (<ul className={css.friendList}>
-        {friends.map(friend => <FriendListItem friend = {friend} />)}
+            {friends.map(friend=><FriendListItem
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+            key={friend.id} />)}
 </ul>)
 }
